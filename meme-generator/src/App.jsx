0a3873a4562d0eed0a18062b0354497b0fdac519
Chickenbiscuitoo <chickenbiscuito.dev@gmail.com>
2022-07-04 17:51:46 +0200

    foundation

---
+++ meme-generator/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css';
+
+import React from 'react';
+import Header from "./components/Header"
+import Meme from "./components/Meme"
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Meme />
+    </div>
+  );
+}
+
+export default App;
---
+++ meme-generator/src/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+export default function Header() {
+    return (
+        <header className="header">
+            <img 
+                src="./images/troll-face.png" 
+                className="header--image"
+            />
+            <h2 className="header--title">Meme Generator</h2>
+        </header>
+    )
+}
